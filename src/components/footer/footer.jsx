a3cfa4072c22782d0c7eb3eb11d94dfc87aafc9e
saika <93665867+saikadayo@users.noreply.github.com>
2026-02-26 21:59:27 -0600

    Changed Footer details

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
       <Container fluid="md">
         <Row>
           <Col>
-            <h5 className="footer-title">Jessica Phu</h5>
+            <h5 className="footer-title">Achievement 3</h5>
             <p className="footer-text">
               A movie collection of various Korean dramas.
             </p>
